3a6695fb7561ac02bc09581d9568ee786b7c005e
r45r54r45 <r45r54r45@yonsei.ac.kr>
2016-12-29 04:29:50 +0900

    dd

--- libs/redis.js
+++ libs/redis.js
@@ -1,6 +1,6 @@
 var redisServer = require('redis');
 var redisClient = (process.env.NODE_ENV=='production'?redisServer.createClient({
-    host: '104.199.216.149',
+    host: '0.0.0.0',
     port: 6379
 }):redisServer.createClient({
     host: 'localhost',
